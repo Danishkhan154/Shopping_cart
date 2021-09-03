7fd45c2ed23777ffff730648c08c9720ec4fa2ea
Danish Khan <danishkhankrz@gmail.com>
2021-09-04 02:43:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Basket from "./Basket";
+import "./index.css";
+import data from "./data";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const { products } = data;
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header countCartItems={cartItems.length} />
+      <div className="row">
+        <Main onAdd={onAdd} products={products} />
+        <Basket onAdd={onAdd} onRemove={onRemove} cartItems={cartItems} />
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Mackbook",
+      price: 54000,
+      image:
+        "https://images.unsplash.com/photo-1602080858428-57174f9431cf?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bGFwdG9wfHx8fHx8MTYzMDY4OTgwMQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
+    },
+
+    {
+      id: "2",
+      name: "Phone",
+      price: 5400,
+      image:
+        "https://images.unsplash.com/photo-1480694313141-fce5e697ee25?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8cGhvbmV8fHx8fHwxNjMwNjkzMzgx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
+    },
+
+    {
+      id: "3",
+      name: "Car",
+      price: 5400,
+      image:
+        "https://images.unsplash.com/photo-1514053514818-a89e7ce304e9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8Y2FyfHx8fHx8MTYzMDY5MzQ0NQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600",
+    },
+  ],
+};
+
+export default data;
